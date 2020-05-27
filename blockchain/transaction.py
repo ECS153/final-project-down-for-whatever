17509d76a63828983a7933aac40d7b5ae8c1f06c
Ben Rausch <slimeguy46@gmail.com>
2020-05-26 20:30:18 -0700

    remove old files

--- blockchain/transaction.py
+++ blockchain/transaction.py
@@ -7,8 +7,8 @@ class Transaction:
     """
     # If we have time, we will use proof of work to make it cost time to generate a transaction.
     # 1 transaction should cost about 10 seconds of computation.
-    # Good side effect is that it also salts, preventing someone from spamming transactions or quickly copying someone
-    # else's transaction and putting their name as the author's name. (well, that's still an issue, but it's harder)
+    # Good side effect is that proof of work also salts. It would make it harder to quickly copying someone
+    # else's transaction and putting their name as the author's name.
     #
     #
     # :param author: an RSA public key
@@ -19,9 +19,9 @@ class Transaction:
     HASH_METHOD = 'SHA-256'
     #leading_zeros_in_valid_hash = 7
 
-    def __init__(self, author: rsa.PublicKey, timestamped_message: TimestampedMessage, signature: bytes):
+    def __init__(self, author: rsa.PublicKey, timestamped_msg: TimestampedMessage, signature: bytes):
         self.author = author
-        self.timestamped_message = timestamped_message
+        self.timestamped_msg = timestamped_msg
         #self.salt = salt
         self.signature = signature
     
@@ -29,17 +29,17 @@ class Transaction:
     def create_now_with_keys(pub: rsa.PublicKey, priv: rsa.PrivateKey, body: bytes):
         author = pub
         timestamp = time.time_ns()
-        timestamped_message = TimestampedMessage(body, timestamp)
-        signature = rsa.sign(timestamped_message.to_bytes(), priv, Transaction.HASH_METHOD)
-        return Transaction(author, timestamped_message, signature)
+        timestamped_msg = TimestampedMessage(body, timestamp)
+        signature = rsa.sign(timestamped_msg.to_bytes(), priv, Transaction.HASH_METHOD)
+        return Transaction(author, timestamped_msg, signature)
     
     def verify(self, timestamp_of_latest_block: int) -> bool:
         try:
-            rsa.verify(self.timestamped_message.to_bytes(), self.signature, self.author)
+            rsa.verify(self.timestamped_msg.to_bytes(), self.signature, self.author)
         except rsa.VerificationError:
             return False
         now = time.time_ns()
-        return self.timestamped_message.timestamp_in_left_open_interval(
+        return self.timestamped_msg.timestamp_in_left_open_interval(
             timestamp_of_latest_block,
             now
         )
@@ -47,5 +47,5 @@ class Transaction:
     def hash(self):
         sha_summer = hashlib.sha256()
         sha_summer.update(self.author)
-        sha_summer.update(self.timestamped_message.to_bytes())
+        sha_summer.update(self.timestamped_msg.to_bytes())
         return sha_summer.digest()
